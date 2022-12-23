04b41c23cc60ffa32dc827a5dbe8108481b34da3
Rosemberg <rosembergjll@gmail.com>
2022-12-23 06:31:18 -0400

    docs agregados

--- src/helpers/getEnvironments.js
+++ src/helpers/getEnvironments.js
@@ -1,8 +1,9 @@
 
 export const getEnvironments  = () => { 
-  import.meta.env;
+  // import.meta.env;
 
   return { 
-    ...import.meta.env
+    // ...import.meta.env
+    VITE_API_URL: import.meta.env.VITE_API_URL
   }
 }
